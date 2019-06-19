1bef9320db1fbcd1d7e93c10ed77d7440d3bf038
Christopher Jordan-Denny <jordan.denny5@gmail.com>
2019-06-18 22:53:43 -0500

    Updated to GPLv3
    Updated the license to GPLv3. This way nobody can steal my code :)

--- tests/test.c
+++ tests/test.c
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the lsm9ds1 library (https://github.com/ChristopherJD/lsm9ds1.git).
+ * Copyright (c) 2019 Christopher Jordan-Denny.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /*
  *  Simple example of a CUnit unit test.
  *
